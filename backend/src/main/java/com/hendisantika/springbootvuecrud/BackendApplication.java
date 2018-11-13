4c850e2b302bd8ffceec0b6be7901f8187b414b4
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-14 06:04:46 +0700

    #4 - Add Backend service module

---
+++ backend/src/main/java/com/hendisantika/springbootvuecrud/BackendApplication.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootvuecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-vue-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/11/18
+ * Time: 06.03
+ */
+
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
